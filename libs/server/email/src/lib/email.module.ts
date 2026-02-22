bdd261b544b2617b727c9a0f3db6e138bccc52c1
WonjaeKim <wonjae.kim@superstart.co.kr>
2026-02-22 15:15:32 +0900

    fix: EmailModule에 ConfigModule import 추가하여 ConfigService 의존성 에러 수정

--- libs/server/email/src/lib/email.module.ts
+++ libs/server/email/src/lib/email.module.ts
@@ -1,12 +1,15 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EmailService } from './email.service';
 
 /**
  * 이메일 서비스 글로벌 모듈.
  * @Global()로 등록하여 어디서든 EmailService를 주입받을 수 있다.
+ * ConfigModule을 import하여 EmailService에서 ConfigService를 주입받을 수 있도록 한다.
  */
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [EmailService],
   exports: [EmailService],
 })
